a5bf1f5274dac632c72299aa6e210f5bea0b0fff
Valeria <valeriia.chirkova@gmail.com>
2021-05-05 10:20:38 +0300

    update

--- DigitalRBOt.py
+++ DigitalRBOt.py
@@ -1,4 +1,7 @@
 import random
+import os
+from flask import Flask, request
+import logging
 
 # Подключаем модуль для Телеграма
 import telebot
@@ -93,8 +96,27 @@ def callback_worker(call):
        bot.send_animation(call.from_user.id, vs)
 
 
+# # Запускаем постоянный опрос бота в Телеграме
+# bot.polling(none_stop=True, interval=0)
+# print('done')
 
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-# Запускаем постоянный опрос бота в Телеграме
-bot.polling(none_stop=True, interval=0)
-print('done')
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://secure-dawn-49690.herokuapp.com/")
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
